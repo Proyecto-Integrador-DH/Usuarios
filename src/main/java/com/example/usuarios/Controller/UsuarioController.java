1c36f9a4e06cb99356dd45dc6128305dc486b6ab
Adriana Paredes Salinas <adrianaparedessalinas@gmail.com>
2024-03-02 18:59:36 -0500

    Verificación de correo

--- src/main/java/com/example/usuarios/Controller/UsuarioController.java
+++ src/main/java/com/example/usuarios/Controller/UsuarioController.java
@@ -31,16 +31,19 @@ public class UsuarioController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UsuarioDTO usuarioDTO) {
-        String email = usuarioDTO.getEmail();
-        String pass = usuarioDTO.getPass();
-
-        String token = authenticationService.authenticate(email, pass);
-
         try {
-            if (token != null) {
-                return ResponseEntity.ok(token);
+            UsuarioDTO usuarioValido = usuarioService.getUsuario(usuarioDTO.getEmail());
+            if (usuarioValido != null) {
+                String email = usuarioDTO.getEmail();
+                String pass = usuarioDTO.getPass();
+                String token = authenticationService.authenticate(email, pass);
+                if (token != null) {
+                    return ResponseEntity.ok(token);
+                } else {
+                    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas.");
+                }
             } else {
-                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Credenciales inválidas.");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró ningún usuario con el email especificado.");
             }
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Hubo un error al procesar la solicitud." + e.getMessage());
